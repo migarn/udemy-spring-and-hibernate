98a8a8adfe4cd07caa1c3fd63e78a423c3cb2a45
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-15 18:47:33 +0200

    Video 27 done

---
+++ spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Keep running for 45 minutes";	
+	}
+
+}
