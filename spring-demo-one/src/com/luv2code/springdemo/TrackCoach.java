c52ee0042b942f1e9e15655c154bc2c706a2fc0e
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-17 17:41:20 +0200

    Video 32 done

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,16 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+	
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -9,7 +19,6 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Just Do It: " + fortuneService.getFortune();
 	}
 }
