5a1af1db3e617b2700d26a32bca701cd7396ad10
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-22 19:42:55 +0200

    Video 39 done

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -13,6 +13,8 @@ public class SetterDemoApp {
 		
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		
 		context.close();
 
