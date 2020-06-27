68d3b944f4ce3782d14a4b70e6491ea2186ca9b9
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-27 17:39:32 +0200

    Video 50 done

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -21,4 +21,12 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStarupStuff");
+	}
+	
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+	}
 }
