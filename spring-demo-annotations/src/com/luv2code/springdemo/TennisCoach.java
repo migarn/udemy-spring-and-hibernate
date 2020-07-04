60d172c6153f588d0456d983645531e77a828bc2
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-04 12:11:43 +0200

    Video 70 done

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,22 +1,25 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
-	@Autowired
-	public void doSomeCrazyStuff(FortuneService theFortuneService) {
-		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-		fortuneService = theFortuneService;
-	}
+//	@Autowired
+//	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//		fortuneService = theFortuneService;
+//	}
 	
 //	@Autowired
 //	public TennisCoach(FortuneService theFortuneService) {
