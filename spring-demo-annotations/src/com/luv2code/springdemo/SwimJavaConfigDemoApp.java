5aed8e50a55d010b64f759cf406cfb23672d2cc7
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-13 19:47:03 +0200

    Video 95 done

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -8,11 +8,14 @@ public class SwimJavaConfigDemoApp {
 		
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println("email: " + theCoach.getEmail());
+		System.out.println("team: " + theCoach.getTeam());
 		
 		context.close();
 
