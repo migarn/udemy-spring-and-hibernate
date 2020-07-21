3a21085dfb1ec13333707ac064c4c2a5ed32518d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-21 08:19:49 +0200

    Video 124 done

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 	
 	@RequestMapping("/showForm")
@@ -26,6 +28,17 @@ public class HelloWorldController {
 		String theName = request.getParameter("studentName");
 		theName = theName.toUpperCase();
 		String result = "Yo! " + theName;
+		model.addAttribute("message", result);
+		
+		return "helloWorld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, Model model) {
+		
+		theName = theName.toUpperCase();
+		String result = "Hey My Friend from v3! " + theName;
 		model.addAttribute("message", result);
 		
 		return "helloWorld";
