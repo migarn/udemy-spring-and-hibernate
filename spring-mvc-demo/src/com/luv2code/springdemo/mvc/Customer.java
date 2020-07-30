48b3e3c52b3a8fbff7d088ff750d305f2dd032fc
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-30 17:53:04 +0200

    Video 155 done

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,18 +13,35 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than of equal to 10")
+	private int freePasses;
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+	
+	
 	
 	
 
