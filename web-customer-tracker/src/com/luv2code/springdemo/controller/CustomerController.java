0ae37ecc9b72e2bce723b600dcf98c103ac2b7c5
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-09 17:41:50 +0200

    Video 302 done

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
@@ -36,5 +38,11 @@ public class CustomerController {
 		theModel.addAttribute("customer", theCustomer);
 		
 		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);		
+		return "redirect:/customer/list";
 	}
 }
