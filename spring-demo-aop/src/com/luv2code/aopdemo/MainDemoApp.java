372bd747391e6f1b3c3fd667559864aa9d5dce09
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-17 18:02:39 +0200

    Video 324 done

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		theAccountDAO.addAccount();
+		
+		System.out.println("\n let's call it again!");
+		theAccountDAO.addAccount();
+		
+		context.close();
+
+	}
+
+}
