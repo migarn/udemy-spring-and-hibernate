96b96ddcb0608e45e094f6418d0b2fb3ec76deee
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-16 17:08:46 +0100

    Video 484 done

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -45,15 +45,4 @@ public class StudentRestController {
 		
 		return theStudents.get(studentId);
 	}
-	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
 }
