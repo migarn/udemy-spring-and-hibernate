a6ce81b91b570f084614f4fb7c88e7051a09b0cc
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-22 19:39:22 +0100

    Video 500 done

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -26,6 +26,11 @@ public class CustomerRestController {
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer is not found - " + customerId);
+		}
+		
 		return theCustomer;
 	}
 
